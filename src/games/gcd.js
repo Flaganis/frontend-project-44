84e55fc9085f3103bcd6c78bf3e6630f9c9d0959
Flaganis <25443471+Flaganis@users.noreply.github.com>
2025-11-18 20:07:38 +0300

    Create gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import getRandomNumber from '../utils/randomNumber.js';
+import run from '../gameEngine.js';
+
+const maxNumb = 100;
+
+const findGCD = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return findGCD(b, a % b);
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber(maxNumb);
+  const num2 = getRandomNumber(maxNumb);
+
+  const lesserNum = Math.min(num1, num2);
+  const biggerNum = Math.max(num1, num2);
+  const question = `${biggerNum} ${lesserNum}`;
+
+  const gcd = findGCD(lesserNum, biggerNum);
+  return [question, String(gcd)];
+};
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+export default () => run(generateRound, rule);
