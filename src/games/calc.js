e0f9912e30161aae42e08ac4b413e00533917c3e
Flaganis <25443471+Flaganis@users.noreply.github.com>
2025-11-18 20:06:36 +0300

    Create calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,25 @@
+import getRandomNumber from '../utils/randomNumber.js';
+import run from '../gameEngine.js';
+
+const maxNumb = 20;
+
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+const symbols = Object.keys(operations);
+
+const generateRound = () => {
+  const num1 = getRandomNumber(maxNumb);
+  const num2 = getRandomNumber(maxNumb);
+  const symbol = symbols[getRandomNumber(symbols.length - 1)];
+  const expression = `${num1} ${symbol} ${num2}`;
+  const answer = String(operations[symbol](num1, num2));
+  return [expression, answer];
+};
+
+const rule = 'What is the result of the expression?';
+
+export default () => run(generateRound, rule);
