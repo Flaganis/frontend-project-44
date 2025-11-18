e966111dc79edd999f4ca2adde90fcd447cacb61
Flaganis <25443471+Flaganis@users.noreply.github.com>
2025-11-18 20:09:49 +0300

    Create progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,38 @@
+import getRandomNumber from '../utils/randomNumber.js';
+import run from '../gameEngine.js';
+
+const maxFirstElem = 20;
+const minStep = 1;
+const maxStep = 10;
+const progressionLength = 10;
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  let next = start;
+
+  for (let i = 0; i < length; i += 1) {
+    progression.push(next);
+    next += step;
+  }
+
+  return progression;
+};
+
+const generateRound = () => {
+  const start = getRandomNumber(maxFirstElem);
+  const step = getRandomNumber(minStep, maxStep);
+  const hiddenNumberIndex = getRandomNumber(0, progressionLength - 1);
+
+  const progressionArray = generateProgression(start, step, progressionLength);
+
+  const answer = String(progressionArray[hiddenNumberIndex]);
+  progressionArray[hiddenNumberIndex] = '..';
+
+  const question = progressionArray.join(' ');
+
+  return [question, answer];
+};
+
+const rule = 'What number is missing in the progression?';
+
+export default () => run(generateRound, rule);
